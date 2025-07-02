bc0c09aebccf5a0b35b1f6adcc9ed53a69616a10
JTSG1 <jamesf3@gmail.com>
2025-07-02 18:22:47 +0100

    feat: added custom basic service definition

---
+++ greyhoundDashboard/services/basic_services/service_custom.py
@@ -0,0 +1,19 @@
+from services.service_base import ServiceBase
+
+
+class ServiceProxmox(ServiceBase):
+
+    """
+    Custom!
+    """
+    id:str = 'custom'
+    name:str = 'Custom'
+    description:str = 'To be used for custom services that do not fit into the other definitions.'
+    tags: list[str] = ['custom']
+
+    def get(self) -> ServiceBase:
+
+        super().get()
+
+        return self
+
